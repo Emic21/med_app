c6330c6b6d02d3fd30b23f6ae843a43ad39d2ce4
Emic21 <ecmichaelo@gmail.com>
2025-03-26 06:16:09 -0400

    update the app state

--- server/db.js
+++ server/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const mongoURI =  "mongodb://root:Gfp4BY4LjtNLXky8RbEan77N@172.21.247.185:27017";
+const mongoURI =  "mongodb://root:4NBIGMMVPoU8DH46KQV2CKHT@172.21.243.135:27017";
 
 const connectToMongo = async (retryCount) => {
     const MAX_RETRIES = 3;
